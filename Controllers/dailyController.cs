5fc995d831a559f53bfcd3221fbbf2dbf3cd1d2e
BVSMine <83999852+BVSMine@users.noreply.github.com>
2021-06-08 09:20:37 +0530

    Add files via upload

---
+++ Controllers/dailyController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using covidcrud.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace covidcrud.Controllers
+{
+    public class dailyController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table = new DataTable();
+            string query = @"select * from daily_cases";
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["dailycases"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+        public string Post(daily day)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                string query = @"insert into daily_cases values('"+day.dailyconfirmed+@"')";
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["dailycases"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added Successfully";
+            }
+            catch(Exception)
+            {
+                return "failwd to add";
+            }
+        }
+
+    }
+}
